a468b62ad55979d0287dd4faf435141a7cb034d3
Tyler Hawkins <tylerhawkins@workfront.com>
2020-07-10 14:51:38 -0600

    adds an interactive sanitization example

--- src/Sanitization.js
+++ src/Sanitization.js
@@ -1,42 +1,74 @@
 import React from 'react';
 import sanitize from 'sanitize-html';
 
-export const Sanitization = () => {
-  const example1 = `<p>Hey</p>`
-  const example2 = `<b>This is bold content.</b>`
-  const example3 = `<img src="1" onerror="alert('Gotcha!')" />`
-  const example4 = `<script>alert('hello world')</script>`
+const example1 = `<p>Hey</p>`
+const example2 = `<b>This is bold content.</b>`
+const example3 = `<img src="1" onerror="alert('Gotcha!')" />`
+const example4 = `<script>alert('hello world')</script>`
 
-  const allExamples = [
-    example1,
-    example2,
-    example3,
-    example4,
-  ]
+const allExamples = [
+  example1,
+  example2,
+  example3,
+  example4,
+]
 
-  return (
-    <div style={containerStyle}>
-      <h2 style={headingStyle}>Sanitization Example</h2>
-      <table style={tableStyle}>
-        <thead>
-          <tr>
-            <th style={tableStyle}>Input</th>
-            <th style={tableStyle}>Output</th>
-            <th style={tableStyle}>Rendered As</th>
-          </tr>
-        </thead>
-        <tbody>
-          {allExamples.map(text => (
-            <tr key={text}>
-              <td style={tableStyle}>{text}</td>
-              <td style={tableStyle}>{sanitize(text)}</td>
-              <td style={tableStyle} dangerouslySetInnerHTML={{ __html: sanitize(text) }} />
+export class Sanitization extends React.Component {
+  state = {
+    searchTerm: '',
+    submittedSearch: '',
+  }
+
+  handleChange = e => {
+    this.setState({ searchTerm: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    this.setState({ submittedSearch: this.state.searchTerm })
+  }
+
+  render() {
+    return (
+      <div style={containerStyle}>
+        <h2 style={headingStyle}>Sanitization Example</h2>
+        <form onSubmit={this.handleSubmit} style={formStyle}>
+          <label style={labelStyle} htmlFor="searchInput">Search</label>
+          <input
+            name="searchInput"
+            id="searchInput"
+            placeholder="Enter HTML Here"
+            value={this.state.searchTerm}
+            onChange={this.handleChange}
+            style={inputStyle}
+          />
+          <button type="submit" style={buttonStyle}>Go</button>
+        </form>
+        {this.state.submittedSearch && (
+          <p style={searchResultsStyle}>Resulting markup is: <span dangerouslySetInnerHTML={{ __html: sanitize(this.state.submittedSearch) }} /></p>
+        )}
+        <table style={tableStyle}>
+          <thead>
+            <tr>
+              <th style={tableStyle}>Input</th>
+              <th style={tableStyle}>Output</th>
+              <th style={tableStyle}>Rendered As</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+          </thead>
+          <tbody>
+            {allExamples.map(text => (
+              <tr key={text}>
+                <td style={tableStyle}>{text}</td>
+                <td style={tableStyle}>{sanitize(text)}</td>
+                <td style={tableStyle} dangerouslySetInnerHTML={{ __html: sanitize(text) }} />
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 const containerStyle = {
@@ -47,6 +79,32 @@ const containerStyle = {
 
 const headingStyle = {
   marginTop: 0,
+}
+
+const formStyle = {
+  marginBottom: '20px',
+}
+
+const labelStyle = {
+  position: 'absolute',
+  left: '-10000px',
+  top: 'auto',
+  width: '1px',
+  height: '1px',
+  overflow: 'hidden',
+}
+
+const inputStyle = {
+  marginRight: '10px',
+  padding: '4px',
+}
+
+const buttonStyle = {
+  padding: '4px',
+}
+
+const searchResultsStyle = {
+  marginBottom: 0,
 }
 
 const tableStyle = {
